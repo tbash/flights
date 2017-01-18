a905c8c12594b0c1a658a3484a52ddd1f5a3943a
T. Brandon Ashley <tb@tba.sh>
2017-01-17 20:37:11 -0500

    feat(*): create require auth fn

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  def require_auth
+    (fb_id, token) = current_token.split(":")
+
+    unless UserService::HasToken.call(fb_id, token)
+      render json: { nothing: true }, status: :unauthorized
+    end
+  end
+
+  def current_token
+    authenticate_with_http_token do |token, _|
+      token
+    end
+  end
 end
